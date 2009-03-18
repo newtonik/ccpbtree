1344990775b6d5d0cb7a72bc5f742337b3a15656
newtonik <newtonik@gmail.com>
2009-03-18 12:10:11 -0400

    continue workin

--- src/btree.h
+++ src/btree.h
@@ -18,6 +18,7 @@
 
 using namespace std;
 
+namespace nwt  {
 //class for the btree
 template <typename _Key, typename _Datatype,int _nodeslots, int _leafslots, typename _Compare = std::less<_Key> >
 class btree {
@@ -29,6 +30,7 @@ public:
 
     typedef  std::pair<_Key, _Datatype> pair_type;
 
+    typedef std::pair<_Key, bool> lookuppair_type;
     //Key comparison function
     typedef  _Compare  key_compare;
 
@@ -133,13 +135,13 @@ private:
 
 	//keep track of the next and previous nodes
 	//They will be helpful for range queries
-	leafNode* nextleaf;
-	leafNode* prevleaf;
+	leafNode* nextLeaf;
+	leafNode* prevLeaf;
 	inline void initialize()
 	{
 	    node::slots = bt_leafnodemax;
 	    node::leafnode = true;
-	    prevleaf=nextleaf = NULL;
+	    prevLeaf=nextLeaf = NULL;
 	    dataSlots = new data_type[node::slot];
 	    keySlots = new keytype[node::slot];
 	}
@@ -190,6 +192,249 @@ private:
     struct leafngroup
     {
     };
+
+    private:
+    // *** Template Magic to Convert a pair or key/data types to a value_type
+
+    /// \internal For sets the second pair_type is an empty struct, so the
+    /// value_type should only be the first.
+    template <typename value_type, typename pair_type>
+    struct btree_pair_to_value
+    {
+        /// Convert a fake pair type to just the first component
+        inline value_type operator()(pair_type& p) const {
+            return p.first;
+        }
+        /// Convert a fake pair type to just the first component
+        inline value_type operator()(const pair_type& p) const {
+            return p.first;
+        }
+    };
+
+    /// \internal For maps value_type is the same as the pair_type
+    template <typename value_type>
+    struct btree_pair_to_value<value_type, value_type>
+    {
+        /// Identity "convert" a real pair type to just the first component
+        inline value_type operator()(pair_type& p) const {
+            return p;
+        }
+        /// Identity "convert" a real pair type to just the first component
+        inline value_type operator()(const pair_type& p) const {
+            return p;
+        }
+    };
+
+    /// Using template specialization select the correct converter used by the
+    /// iterators
+    typedef btree_pair_to_value<value_type, pair_type> pair_to_value_type;
+
+
+public:
+    // *** Iterators and Reverse Iterators
+
+    class iterator;
+    class const_iterator;
+    class reverse_iterator;
+    class const_reverse_iterator;
+
+    /// STL-like iterator object for B+ tree items. The iterator points to a
+    /// specific slot number in a leaf.
+    class iterator
+    {
+    public:
+        // *** Types
+
+        /// The key type of the btree. Returned by key().
+        typedef typename btree::keytype                key_type;
+
+        /// The data type of the btree. Returned by data().
+        typedef typename btree::data_type               data_type;
+
+        /// The value type of the btree. Returned by operator*().
+        
+        /// The pair type of the btree.
+        typedef typename btree::pair_type               pair_type;
+
+        /// Reference to the value_type. STL required.
+        //typedef value_type&             reference;
+
+        /// Pointer to the value_type. STL required.
+        //typedef value_type*             pointer;
+
+        /// STL-magic iterator category
+        //typedef std::bidirectional_iterator_tag iterator_category;
+
+        /// STL-magic
+        //typedef ptrdiff_t               difference_type;
+
+        /// Our own type
+        typedef iterator                self;
+
+    private:
+        // *** Members
+
+        /// The currently referenced leaf node of the tree
+        typename btree::leafNode*      currnode;
+
+        /// Current key/data slot referenced
+        unsigned short          currslot;
+
+        /// Friendly to the const_iterator, so it may access the two data items directly.
+        friend class const_iterator;
+
+        /// Also friendly to the reverse_iterator, so it may access the two data items directly.
+        friend class reverse_iterator;
+
+        /// Also friendly to the const_reverse_iterator, so it may access the two data items directly.
+        friend class const_reverse_iterator;
+
+        /// Evil! A temporary value_type to STL-correctly deliver operator* and
+        /// operator->
+        //mutable value_type              temp_value;
+
+        // The macro BTREE_FRIENDS can be used by outside class to access the B+
+        // tree internals. This was added for wxBTreeDemo to be able to draw the
+        // tree.
+        
+    public:
+        // *** Methods
+
+        /// Default-Constructor of a mutable iterator
+        inline iterator()
+            : currnode(NULL), currslot(0)
+        { }
+
+        /// Initializing-Constructor of a mutable iterator
+        inline iterator(typename btree::leaf_node *l, unsigned short s)
+            : currnode(l), currslot(s)
+        { }
+
+        /// Copy-constructor from a reverse iterator
+        inline iterator(const reverse_iterator &it)
+            : currnode(it.currnode), currslot(it.currslot)
+        { }
+
+        /// Dereference the iterator, this is not a value_type& because key and
+        /// value are not stored together
+        inline reference operator*() const
+        {
+            temp_value = pair_to_value_type()( pair_type(currnode->keySlots[currslot],
+                                                         currnode->dataSlots[currslot]) );
+            return temp_value;
+        }
+
+        /// Dereference the iterator. Do not use this if possible, use key()
+        /// and data() instead. The B+ tree does not stored key and data
+        /// together.
+        inline pointer operator->() const
+        {
+            temp_value = pair_to_value_type()( pair_type(currnode->keySlots[currslot],
+                                                         currnode->dataSlots[currslot]) );
+            return &temp_value;
+        }
+
+        /// Key of the current slot
+        inline const key_type& key() const
+        {
+            return currnode->keySlots[currslot];
+        }
+
+        /// Writable reference to the current data object
+        inline data_type& data() const
+        {
+            return currnode->dataSlots[currslot];
+        }
+
+        /// Prefix++ advance the iterator to the next slot
+        inline self& operator++()
+        {
+            if (currslot + 1 < currnode->slotsinuse) {
+                ++currslot;
+            }
+            else if (currnode->nextLeaf != NULL) {
+                currnode = currnode->nextLeaf;
+                currslot = 0;
+            }
+            else {
+                // this is end()
+                currslot = currnode->slotsinuse;
+            }
+
+            return *this;
+        }
+
+        /// Postfix++ advance the iterator to the next slot
+        inline self operator++(int)
+        {
+            self tmp = *this;   // copy ourselves
+
+            if (currslot + 1 < currnode->slotsinuse) {
+                ++currslot;
+            }
+            else if (currnode->nextLeaf != NULL) {
+                currnode = currnode->nextLeaf;
+                currslot = 0;
+            }
+            else {
+                // this is end()
+                currslot = currnode->slotsinuse;
+            }
+
+            return tmp;
+        }
+
+        /// Prefix-- backstep the iterator to the last slot
+        inline self& operator--()
+        {
+            if (currslot > 0) {
+                --currslot;
+            }
+            else if (currnode->prevLeaf != NULL) {
+                currnode = currnode->prevLeaf;
+                currslot = currnode->slotsinuse - 1;
+            }
+            else {
+                // this is begin()
+                currslot = 0;
+            }
+
+            return *this;
+        }
+
+        /// Postfix-- backstep the iterator to the last slot
+        inline self operator--(int)
+        {
+            self tmp = *this;   // copy ourselves
+
+            if (currslot > 0) {
+                --currslot;
+            }
+            else if (currnode->prevLeaf != NULL) {
+                currnode = currnode->prevLeaf;
+                currslot = currnode->slotsinuse - 1;
+            }
+            else {
+                // this is begin()
+                currslot = 0;
+            }
+
+            return tmp;
+        }
+
+        /// Equality of iterators
+        inline bool operator==(const self& x) const
+        {
+            return (x.currnode == currnode) && (x.currslot == currslot);
+        }
+
+        /// Inequality of iterators
+        inline bool operator!=(const self& x) const
+        {
+            return (x.currnode != currnode) || (x.currslot != currslot);
+        }
+    };
+
     //public methods
 public:
     node* root;
@@ -215,6 +460,7 @@ public:
      * Delete key, data pair from index 
      */
     void delete_pair(keytype k, data_type data);
+
 
     /**
      *Find Key in tree.
@@ -254,12 +500,11 @@ public:
 
 
 	}	
-	//if we are out of the loop it means we have reached a leaf node
+	//if we are out of the loop it means we have reached a leaf node, return false
 	for(i = 0; i < curNode.keyCount(); i++)
 	{
 	    if(key_compare(curNode->keySlots[i], k) == 0)
 	    {
-		TREE_PRINT("Found key at key slot " << i);
 		return true;
 	    }
 	}
@@ -267,11 +512,26 @@ public:
 
 	return false;
     }
+    /**
+     *  just calls find and returns a bool the key exists
+     **/
+    inline bool exists(keytype k)
+    {
+        if(find(k) != null)
+            return true;
 
+        return false;
+
+    }
+    inline bool insert(keytype k, data_type data)
+    {
+
+
+    }
 private:
 
     void insert_in_parent(node* N, keytype k, node* Nprime);
 };
 
-
+}
 #endif
